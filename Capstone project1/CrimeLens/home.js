a4b2c069ba246f6a6dea8dbbf509255a828f2c3c
RRahul05 <rahulrajbth2023@gmail.com>
2025-12-12 11:55:50 +0530

    Add files via upload

---
+++ Capstone project1/CrimeLens/home.js
@@ -0,0 +1,12 @@
+function exploreLinks() {
+    window.scrollTo({ 
+        top: 800, 
+        behavior: "smooth" 
+    });
+}
+
+
+document.getElementById("myCard").addEventListener("click", function() {
+        window.open("central-links.html", "_blank");
+
+});
